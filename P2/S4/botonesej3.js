193c62c616627a8405d7260c208a8f12c3908690
cviloria <cv.viloria.2020@alumnos.urjc.es>
2023-02-28 08:53:51 +0000

    cambios

--- P2/S4/botonesej3.js
+++ P2/S4/botonesej3.js
@@ -3,23 +3,33 @@ console.log("Ejecutando JS...");
 //-- Crear objeto gui, con los elementos de la interfaz gráfica
 //-- Al tenerlo agrupado podemos pasarlo como parámetro o asignarlo
 //-- a otro objeto
-const gui = {
+/* const gui = {
   display: document.getElementById("display"),
   boton: document.getElementById("boton"),
+} */
+
+const gui = {
+  display: document.getElementById("display"),
+  boton_inc: document.getElementById("boton_inc"),
+  boton_dec: document.getElementById("boton_dec")
 }
 
-//-- Objeto contador: Contiene el valor y el método para incrementarse (usamos el metodo en vez d hacerlo directamente como antes)
+//-- Objeto contador: Contiene el valor y el método para incrementarse
 const counter = {
   valor: 0,
   inc : function(value) {
-    this.valor += value ;
+    this.valor += value;
     gui.display.innerHTML = this.valor;
   }
 }
-// si queremos hacer una operacion con el display usamos el objeto gui q hemos declardo
-//gui.boton
 
-//-- Acciones: Ligar el botón al contador
-gui.boton.onclick = () => {
-  counter.inc(1)
+//-------- Accciones:
+//-- Incrementar contador
+gui.boton_inc.onclick = () => {
+  counter.inc(1);
+}
+
+//-- Decrementar contador
+gui.boton_dec.onclick = () =>{
+  counter.inc(-1);
 }
