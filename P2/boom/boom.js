8e1670cc8723bbed9e4504a4fc7531c10c544d0e
cviloria <cv.viloria.2020@alumnos.urjc.es>
2023-03-14 09:44:14 +0100

    textos html

--- P2/boom/boom.js
+++ P2/boom/boom.js
@@ -1,3 +1,23 @@
+console.log("Ejecuitando JS...");
+
+//-- Elementos de la interfaz del juego
+display1 = document.getElementById("display1")
+display2 = document.getElementById("display2")
+display3 = document.getElementById("display3")
+display4 = document.getElementById("display4")
+
+boton1 = document.getElementById("boton1")
+boton2 = document.getElementById("boton2")
+boton3 = document.getElementById("boton3")
+boton4 = document.getElementById("boton4")
+boton5 = document.getElementById("boton5")
+boton6 = document.getElementById("boton6")
+boton7 = document.getElementById("boton7")
+boton8 = document.getElementById("boton8")
+boton9 = document.getElementById("boton9")
+boton0 = document.getElementById("boton0")
+
+
 //-- Elementos de la gui
 const gui = {
     display : document.getElementById("display"),
@@ -6,7 +26,7 @@ const gui = {
     reset : document.getElementById("reset")
 }
 
-console.log("Ejecuitando JS...");
+
 
 //-- Definir un objeto cronómetro
 const crono = new Crono(gui.display);
