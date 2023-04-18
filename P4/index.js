e1a002bf76701fb3cbb3bb40a540b3e56d5eccb9
cviloria <cv.viloria.2020@alumnos.urjc.es>
2023-04-18 18:46:28 +0200

    cambios y fotos subidas

--- P4/index.js
+++ P4/index.js
@@ -1,1 +1,296 @@
-console.log()
+console.log("Ejecutando JS...");
+const selectors = {
+    gridContainer: document.querySelector('.grid-container'),
+    tablero: document.querySelector('.tablero'),
+    movimientos: document.querySelector('.movimientos'),
+    timer: document.querySelector('.timer'),
+    comenzar: document.querySelector('button'),
+    win: document.querySelector('.win')
+}
+
+//text 3 las opciones de dimensiones 
+const text3 = document.getElementById("text3");
+const text3_disp = document.getElementById("text3_disp");
+text3.onchange = () => {
+    text3_disp.innerHTML = text3.value; 
+}
+const r1 = document.getElementById("2");
+const r2 = document.getElementById("4");
+const r3 = document.getElementById("6");
+const r_disp = document.getElementById("radio_display");
+
+r1.onchange = () => {
+    r_disp.innerHTML = "2";   
+}
+
+r2.onchange = () => {
+    r_disp.innerHTML = "4";   
+}
+
+r3.onchange = () => {
+    r_disp.innerHTML = "6";   
+}
+
+
+const state = {
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null
+}
+ 
+
+const generateGame = () => {
+    const dimensions = selectors.tablero.getAttribute('grid-dimension')
+
+    //-- Nos aseguramos de que el número de dimensiones es par
+    // y si es impar lanzamos un error
+    if (dimensions % 2 !== 0) {
+        throw new Error("Las dimensiones del tablero deben ser un número par.")
+    }
+
+    //-- Creamos un array con los emojis que vamos a utilizar en nuestro juego
+   
+    
+    let emojis = []; //array donde van los personajes
+    function cargarImagenes(){
+    emojis[0] = "donna.jpg";
+    emojis[1] = "eric.jpg";
+    emojis[2] = "fez.jpg";
+    emojis[3] = "hyde.png";
+    emojis[4] = "jackie.jpg";
+    emojis[5] = "kelso.jpg";
+    emojis[6] = "kitty.jpg";
+    emojis[7] = "Leo.webp";
+    emojis[8] = "red.webp";
+    emojis[9] = "logo.png";
+    emojis[10] = "bob.JPEG";
+    emojis[11] = "laurie.jpg";
+    emojis[12] ="tanyaroberts.jpg";
+    emojis[13] = "Fenton.webp";
+    emojis[14] = "corvette.jpg";
+    emojis[15] = "skating.jpg";
+    emojis[16] = "milaashton.jpg";
+    emojis[17] = "donnaeric.jpg"
+    }
+    cargarImagenes();
+    //-- Elegimos un subconjunto de emojis al azar, así cada vez que comienza el juego
+    // es diferente.
+    // Es decir, si tenemos un array con 10 emojis, vamos a elegir el cuadrado de las
+    // dimensiones entre dos, para asegurarnos de que cubrimos todas las cartas
+    const picks = pickRandom(emojis, (dimensions * dimensions) / 2) 
+
+    //-- Después descolocamos las posiciones para asegurarnos de que las parejas de cartas
+    // están desordenadas.
+    const items = shuffle([...picks, ...picks])
+    
+    //-- Vamos a utilizar una función de mapeo para generar 
+    //  todas las cartas en función de las dimensiones
+    const cards = `
+        <div class="tablero" style="grid-template-columns: repeat(${dimensions}, auto)">
+            ${items.map(item => `
+                <div class="card">
+                    <div class="card-front"></div>
+                    <div class="card-back"><img src="${item}" id="imagenes"></div>
+                </div>
+            `).join('')}
+       </div>
+    `
+    
+    //-- Vamos a utilizar un parser para transformar la cadena que hemos generado
+    // en código html.
+    const parser = new DOMParser().parseFromString(cards, 'text/html')
+
+    //-- Por último, vamos a inyectar el código html que hemos generado dentro de el contenedor
+    // para el tablero de juego.
+    selectors.tablero.replaceWith(parser.querySelector('.tablero'))
+}
+
+const pickRandom = (array, items) => {
+    // La sintaxis de tres puntos nos sirve para hacer una copia del array
+    const clonedArray = [...array]
+    // Random picks va almacenar la selección al azar de emojis
+    const randomPicks = [] 
+
+    for (let index = 0; index < items; index++) {
+        const randomIndex = Math.floor(Math.random() * clonedArray.length)
+        // Utilizamos el índice generado al azar entre los elementos del array clonado
+        // para seleccionar un emoji y añadirlo al array de randompicks.
+        randomPicks.push(clonedArray[randomIndex])
+        // Eliminamos el emoji seleccionado del array clonado para evitar que 
+        // vuelva a salir elegido con splice.
+        // 0 - Inserta en la posición que le indicamos.
+        // 1 - Remplaza el elemento, y como no le damos un nuevo elemento se queda vacío.
+        clonedArray.splice(randomIndex, 1)
+    }
+
+    return randomPicks
+}
+const shuffle = array => {
+    const clonedArray = [...array]
+
+    // Intercambiamos las posiciones de los emojis al azar para desorganizar el array
+    // así nos aseguramos de que las parejas de emojis no están consecutivas.
+    // Para conseguirlo utilizamos un algoritmo clásico de intercambio y nos apoyamos
+    // en una variable auxiliar.
+    for (let index = clonedArray.length - 1; index > 0; index--) {
+        const randomIndex = Math.floor(Math.random() * (index + 1))
+        const original = clonedArray[index]
+
+        clonedArray[index] = clonedArray[randomIndex]
+        clonedArray[randomIndex] = original
+    }
+
+    return clonedArray
+}
+const attachEventListeners = () => {
+    document.addEventListener('click', event => {
+        // Del evento disparado vamos a obtener alguna información útil
+        // Como el elemento que ha disparado el evento y el contenedor que lo contiene
+        const eventTarget = event.target
+        const eventParent = eventTarget.parentElement
+
+        // Cuando se trata de una carta que no está girada, le damos la vuelta para mostrarla
+        if (eventTarget.className.includes('card') && !eventParent.className.includes('flipped')) {
+            flipCard(eventParent)
+        // Pero si lo que ha pasado es un clic en el botón de comenzar lo que hacemos es
+        // empezar el juego
+        } else if (eventTarget.nodeName === 'BUTTON' && !eventTarget.className.includes('disabled')) {
+            startGame()
+        }
+    })
+}
+
+// Generamos el juego
+generateGame()
+
+// Asignamos las funciones de callback para determinados eventos
+attachEventListeners()
+
+const startGame = () => {
+    // Iniciamos el estado de juego
+    state.gameStarted = true
+    // Desactivamos el botón de comenzar
+    selectors.comenzar.classList.add('disabled')
+
+    // Comenzamos el bucle de juego
+    // Cada segundo vamos actualizando el display de tiempo transcurrido
+    // y movimientos
+    state.loop = setInterval(() => {
+        state.totalTime++
+
+        selectors.movimientos.innerText = `${state.totalFlips} movimientos`
+        selectors.timer.innerText = `tiempo: ${state.totalTime} sec`
+    }, 1000)
+}
+const flipCard = card => {
+    // Sumamos uno al contador de cartas giradas
+    state.flippedCards++
+    // Sumamos uno al contador general de movimientos
+    state.totalFlips++
+
+    // Si el juego no estaba iniciado, lo iniciamos
+    if (!state.gameStarted) {
+        startGame()
+    }
+
+    // Si no tenemos la pareja de cartas girada
+    // Giramos la carta añadiendo la clase correspondiente
+    if (state.flippedCards <= 2) {
+        card.classList.add('flipped')
+    }
+
+    // Si ya tenemos una pareja de cartas girada tenemos que comprobar
+    if (state.flippedCards === 2) {
+        // Seleccionamos las cartas que están giradas
+        // y descartamos las que están emparejadas
+        const flippedCards = document.querySelectorAll('.flipped:not(.matched)')
+
+        // Si las cartas coinciden las marcamos como pareja 
+        // añadiendo la clase correspondiente
+        if (flippedCards[0].innerHTML === flippedCards[1].innerHTML) {
+            flippedCards[0].classList.add('matched')
+            flippedCards[1].classList.add('matched')
+        }
+
+        // Arrancamos un temporizador que comprobará si tiene
+        // que volver a girar las cartas porque no hemos acertado
+        // o las deja giradas porque ha sido un match
+        // y para eso llamamos a la función flipBackCards()
+        setTimeout(() => {
+            flipBackCards()
+        }, 1000)
+    }
+
+    // Antes de terminar, comprobamos si quedan cartas por girar
+    // porque cuando no quedan cartas por girar hemos ganado
+    // y se lo tenemos que mostrar al jugador
+    if (!document.querySelectorAll('.card:not(.flipped)').length) {
+        setTimeout(() => {
+            // Le damos la vuelta al tablero
+            selectors.gridContainer.classList.add('flipped')
+            // Le mostramos las estadísticas del juego
+            selectors.win.innerHTML = `
+                <span class="win-text">
+                    ¡Has ganado!<br />
+                    con <span class="highlight">${state.totalFlips}</span> movimientos<br />
+                    en un tiempo de <span class="highlight">${state.totalTime}</span> segundos
+                </span>
+            `
+            // Paramos el loop porque el juego ha terminado
+            clearInterval(state.loop)
+        }, 1000)
+    }
+}
+const flipBackCards = () => {
+    // Seleccionamos las cartas que no han sido emparejadas
+    // y quitamos la clase de giro
+    document.querySelectorAll('.card:not(.matched)').forEach(card => {
+        card.classList.remove('flipped')
+    })
+    // Ponemos el contado de parejas de cartas a cero
+    state.flippedCards = 0
+}
+
+
+
+// cosas mias...
+/* let personajes = [];
+function cargarImagenes(){
+    personajes[0] = "donna.jpg";
+    personajes[1] = "eric.jpg";
+    personajes[2] = "fez.jpg";
+    personajes[3] = "hyde.png";
+    personajes[4] = "jackie.jpg";
+    personajes[5] = "kelso.jpg";
+    personajes[6] = "kitty.jpg";
+    personajes[7] = "leo.jpg";
+}
+const numImg = 8;
+function imagenes() {
+	let imagenes = [];
+	let i = 0;
+	while (i < 8) {
+		let nuevaImagen = random.personajes; //Esto acaso se puede?
+		if (!imagenes.includes(nuevaImagen)) {
+			imagenes[i] = nuevaImagen;
+			i++;
+		}
+	}
+	return mezclarImagenes(imagenes, numImg);
+}
+function mezclarImagenes(imagenes, numImg) {
+	let baraja = [];
+	baraja.length = numImg * 2;
+
+	let i = 0
+	while (i < baraja.length) {
+		let nuevaImagen = imagenes[getAleatorio(imagenes)];
+		if (!baraja.includes(nuevaImagen) || contarRepeticiones(baraja, nuevaImagen) < 2) {
+			baraja[i] = nuevaImagen;
+			i++;
+		}
+	}
+	return baraja;
+} */
